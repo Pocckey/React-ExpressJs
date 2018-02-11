02f441dcd5431ca7a0f0512e75db0b7759c0f38b
Matthew <mat.cy.leung@hotmail.com>
2018-02-10 22:06:20 -0800

    implement scss and npm run test

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
-import logo from "./logo.svg";
-import "./App.scss";
+import logo from "./img/logo.svg";
 import Child from "./components/TestDiv";
 
 export default class App extends Component {
@@ -17,12 +16,12 @@ export default class App extends Component {
   }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">{this.state.call}</h1>
+      <div className="app">
+        <header className="app-header">
+          <img src={logo} className="app-logo" alt="logo" />
+          <h1 className="app-title">{this.state.call}</h1>
         </header>
-        <p className="App-intro">
+        <p className="app-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <Child />
