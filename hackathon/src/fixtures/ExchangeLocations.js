8e8737362736279c016b8b9850b02e95e1d121cf
Matthew <mat.cy.leung@hotmail.com>
2018-02-16 10:17:21 -0800

    WIP fixtures

---
+++ hackathon/src/fixtures/ExchangeLocations.js
@@ -0,0 +1,44 @@
+export default {
+  listedLocations: [
+    {
+      name: "Everforex Currency Exchange Vancouver Downtown",
+      address: "489 Richards St, Vancouver, BC",
+      zipcode: "V6B6H6",
+    },
+    {
+      name: "TD Canada Trust",
+      address: "700 W Georgia St",
+      zipcode: "V7Y1A2",
+    },
+    {
+      name: "Vancouver Bullion & Currency Exchange",
+      address: "800 W Pender St, Vancouver, BC",
+      zipcode: "V6C2V6",
+    },
+    {
+      name: "Agility Forex",
+      address: "885 W Georgia St #1507, Vancouver, BC",
+      zipcode: "V6C 3E8",
+    },
+    {
+      name: "Kingmark Currency Exchange Inc. - Vancouver",
+      address: "610 W Broadway, Vancouver, BC",
+      zipcode: "V5Z 1G1",
+    },
+    {
+      name: "Happy Currency Exchange Inc",
+      address: "409 W Broadway #101, Vancouver, BC",
+      zipcode: "V5Y 1R4",
+    },
+    {
+      name: "Knightsbridge Foreign Exchange Vancouver",
+      address: "1285 W Broadway #600, Vancouver, BC",
+      zipcode: "V6H 3X8",
+    },
+    {
+      name: "Javan Exchange",
+      address: "819 Davie St, Vancouver, BC",
+      zipcode: "V6Z 1B7",
+    },
+  ],
+};
---
+++ hackathon/src/fixtures/LocationsRates.js
@@ -0,0 +1,26 @@
+export default {
+  LocationsRates: [
+    {
+      name: "Everforex Currency Exchange Vancouver Downtown",
+      AUDUSD: {
+        sellRate: 0.8056,
+        buyRate: 0.7886,
+      },
+      CADGBP: {
+        sellRate: 0.575,
+        buyRate: 0.5595,
+      },
+      USDCAD: {
+        sellRate: 1.2466,
+        buyRate: 1.2431,
+      },
+      CADEUR: {
+        sellRate: 0.6486,
+        buyRate: 0.6333,
+      },
+    },
+    {
+      name: "Vancouver Bullion & Currency Exchange",
+    },
+  ],
+};
